fd69d3bac0ea286119db2f603a53513493ad57d8
Christian Tukunda <christiantukundastocklin@gmail.com>
2024-08-22 14:30:17 +0100

    First commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,13 @@
-// routes/authRoutes.js
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const authMiddleware = require('../middleware/authMiddleware'); // Middleware d'authentification
 
 router.post('/login', authController.login);
+router.post('/register', authController.register);
+
+// Route pour obtenir les informations de l'utilisateur connecté
+router.get('/servers', authController.getAllUserProfile);
+router.get('/me', authController.getUserProfile);
 
 module.exports = router;
