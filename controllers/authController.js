6a4d45c5dbc675e51f3119ab7e52efe91427650b
Christian Tukunda <christiantukundastocklin@gmail.com>
2024-08-22 14:55:21 +0100

    First commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const User = require('../models/User');
-const bcrypt = require('bcrypts');
+const bcrypt = require('bcryptjs');
 const generateToken = require('../utils/generateToken');
 
 // Connexion
