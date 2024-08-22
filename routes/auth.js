bd3a7b664a395e6a647369cb1527298e879d7054
Christian Tukunda <christiantukundastocklin@gmail.com>
2024-08-22 13:44:22 +0100

    First commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 // const express = require('express');
-const { login, register, update, delete: deleteUser } = require('../controllers/authController');
+// const { login, register, update, delete: deleteUser } = require('../controllers/authController');
 // const router = express.Router();
 
 // router.post('/login', login);
@@ -16,7 +16,7 @@ router.post('/login', authController.login);
 router.post('/register', authController.register);
 
 // Route pour obtenir les informations de l'utilisateur connecté
-router.get('/update', authController.update);
-router.get('/delete', authController.delete);
+router.post('/update', authController.update);
+router.post('/delete', authController.delete);
 
 module.exports = router;
