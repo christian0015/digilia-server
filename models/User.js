c0ba779ddf6c6d3535cde6fe5e4314d7bbaa4f8f
Christian Tukunda <christiantukundastocklin@gmail.com>
2024-08-22 14:20:59 +0100

    First commit

--- models/User.js
+++ models/User.js
@@ -4,10 +4,6 @@ const userSchema = new mongoose.Schema({
   username: {type: String,required: true,unique: true,},
   email: {type: String,required: true,unique: true,lowercase: true,},
   password: {type: String,required: true,},
-  role: {type: String,enum: ['admin', 'client'],default: 'client',},
-  subscription: {type: {  type: String,  enum: ['free', 'basic', 'premium'],default: 'free',},startDate: {  type: Date,  default: Date.now,},endDate: {  type: Date,},status: {  type: String,  enum: ['active', 'inactive'],  default: 'inactive',},},
-  createdAt: {type: Date,default: Date.now,},
-  updatedAt: {type: Date,default: Date.now,},
 }); // Nom de collection avec préfixe
 
 userSchema.pre('save', function (next) {
