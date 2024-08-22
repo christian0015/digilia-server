cf217b4797c22a8d98cfea81e0059c27340ebcb1
Christian Tukunda <christiantukundastocklin@gmail.com>
2024-08-22 13:30:49 +0100

    First commit

--- server.js
+++ server.js
@@ -30,9 +30,12 @@ mongoose.connect(config.mongoURI, {
 app.use(express.json());
 
 // Routes
+// app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/products', require('./routes/productRoutes'));
+app.use('/api/ventes', require('./routes/venteRoutes'));
 // Routes
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/projects', require('./routes/projet')); // Assurez-vous que le nom de la route est correct
+// app.use('/api/auth', require('./routes/auth'));
+// app.use('/api/projects', require('./routes/projet')); // Assurez-vous que le nom de la route est correct
 
 
 // Erreur 404
