0b5d6eea307dd882ded7fa12ee8d9e10429a70c4
Christian Tukunda <christiantukundastocklin@gmail.com>
2024-08-22 13:31:52 +0100

    First commit

--- server.js
+++ server.js
@@ -34,8 +34,8 @@ app.use(express.json());
 app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/ventes', require('./routes/venteRoutes'));
 // Routes
-// app.use('/api/auth', require('./routes/auth'));
-// app.use('/api/projects', require('./routes/projet')); // Assurez-vous que le nom de la route est correct
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/projects', require('./routes/projet')); // Assurez-vous que le nom de la route est correct
 
 
 // Erreur 404
