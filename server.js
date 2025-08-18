753159ae62716033a3d4000ee078572bbc473ea9
Christian TUKUNDA <christiantukundastocklin@gmail.com>
2025-08-18 09:23:55 +0200

    Feature Newsletter and Commentaires

--- server.js
+++ server.js
@@ -31,6 +31,9 @@ app.use('/api/projets', require('./routes/projet')); // Assurez-vous que le nom 
 app.use('/api/export', require('./routes/export'));
 
 
+app.use('/api/digilia-info', require('./routes/digiliaInfo'));
+
+
 // Erreur 404
 app.use((req, res, next) => {
   res.status(404).json({ message: 'Resource not found' });
